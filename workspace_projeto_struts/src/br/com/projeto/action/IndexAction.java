b0329d300e629b5e988ba85bc7ffb21af37c16aa
ederson-bsi <ederson.bsi@gmail.com>
2020-10-27 14:06:56 -0300

    Update IndexAction.java

--- workspace_projeto_struts/src/br/com/projeto/action/IndexAction.java
+++ workspace_projeto_struts/src/br/com/projeto/action/IndexAction.java
@@ -16,7 +16,7 @@ public class IndexAction implements Action{
 		System.out.println("novo teste 2");
 		System.out.println("sexto teste");
 		System.out.println("setimo teste");
-		System.out.println("oitavo teste");
+		System.out.println("oitavo teste novo");
 		return SUCCESS;
 	}
 
